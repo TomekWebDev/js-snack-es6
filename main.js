77be66cccc680d212716120071e248bf25a2cc40
TomekWebDev <tommaso.mangiagalli@gmail.com>
2022-11-07 16:54:52 +0100

    esercizio 2 con due varianti

--- main.js
+++ main.js
@@ -26,34 +26,88 @@
 
 // console.log(newArray);
 
-// Esercizio 1 - filter
+// // Esercizio 1 - filter
 
-let myArray = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
+// let myArray = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
 
 
-console.log(myArray.length);
+// console.log(myArray.length);
 
-let min = parseInt(prompt("Inserisci numero minimo"));
+// let min = parseInt(prompt("Inserisci numero minimo"));
 
-let max = parseInt(prompt("Inserisci numero minimo"));
+// let max = parseInt(prompt("Inserisci numero minimo"));
 
-if(min >= 0 && max <= (myArray.length) - 1){
+// if(min >= 0 && max <= (myArray.length) - 1){
 
-    let newArray = myArray.filter((element, index) => {
-        if(index >= min && index <= max){
+//     let newArray = myArray.filter((element, index) => {
+//         if(index >= min && index <= max){
 
-            return true;
+//             return true;
 
-        }
-        return false;
+//         }
+//         return false;
       
         
-    });
-    console.log(newArray);
+//     });
+//     console.log(newArray);
 
-}
-else{
-    alert(`Number must be between 0 and ${myArray.length - 1}`)
-}
+// }
+// else{
+//     alert(`Number must be between 0 and ${myArray.length - 1}`)
+// }
+
+// Esercizio 2
+
+let students = [
+    { name: 'Marco', id: 213, grades: 78 },
+    { name: 'Paola', id: 110, grades: 96 },
+    { name: 'Andrea', id: 250, grades: 48 },
+    { name: 'Gaia', id: 145, grades: 74 },
+    { name: 'Luigi', id: 196, grades: 68 },
+    { name: 'Piero', id: 102, grades: 50 },
+    { name: 'Francesca', id: 120, grades: 84 },
+];
+
+// primo punto
+
+let nameUppercase = students.map((element) => {
+    return element.name.toUpperCase();
+});
+  
+console.log(nameUppercase);
+
+// secondo punto
+
+let over70 = students.filter((element) => {
+    if(element.grades > 70){
+        return true;
+    }
+    return false;
+});
+
+console.log(over70);
+
+// prima modalità per filtrare con 2 "parametri"
+
+let over70over120 = students.filter((element) => {
+    if(element.grades > 70 && element.id > 120){
+        return true;
+    }
+    return false;
+});
+
+console.log(over70over120);
+
+// seconda modalità con includes che lavora sull'array creata dal filtro precedente
+
+let over70over120Variant = students.filter((element) => {
+    if(element.id > 120 && over70.includes(element)){
+        return true;
+    }
+    return false;
+});
+
+console.log(over70over120Variant);
+
 
 
