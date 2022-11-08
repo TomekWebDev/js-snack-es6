dc47156589e30e20c8d9d604b851e273028a52e5
TomekWebDev <tommaso.mangiagalli@gmail.com>
2022-11-08 15:02:56 +0100

    destruct. the array (test)

---
+++ Snack 3/main.js
@@ -0,0 +1,20 @@
+const roadBikes = [
+    {
+        nome: "Pinnarello Dogma F12",
+        peso: 5
+    },
+    {
+        nome: "Trek Emonda",
+        peso: 6
+    },
+    {
+        nome: "Trek Madone",
+        peso: 7
+    }
+];
+
+
+roadBikes.forEach(({nome, peso}) => console.log(nome, peso));
+
+
+
